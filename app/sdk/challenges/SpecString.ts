bf53b838f153323c7ee887d4b46c2dc5cc5d34e8
noedne <ney.brendan@gmail.com>
2023-06-14 13:36:07 -0700

    Add SpecString class for reading binary strings

---
+++ app/sdk/challenges/SpecString.ts
@@ -0,0 +1,66 @@
+export default class SpecString {
+  str: string;
+  index: number = 0;
+
+  constructor(str: string) {
+    this.str = str;
+  }
+
+  matchRegex(regex: RegExp): string | null {
+    const res = regex.exec(this.str);
+    if (res === null) {
+      return null;
+    }
+    this.index += res[0].length;
+    return res[0];
+  }
+
+  countZeroes(): number | null {
+    const match = this.matchRegex(/^0*1/);
+    if (match === null) {
+      return null;
+    }
+    return match.length - 1;
+  }
+
+  readNBits(n: number): number | null {
+    const match = this.matchRegex(new RegExp(`^[01]{${n}}`));
+    if (match === null) {
+      return null;
+    }
+    return parseInt(match, 2);
+  }
+
+  countZeroesAndReadNPlusBits(n: number): number | null {
+    const zeroes = this.countZeroes();
+    if (zeroes === null) {
+      return null;
+    }
+    if (zeroes > 0) {
+      // include the leading 1 in the return value
+      this.index--;
+    }
+    return this.readNBits(n + zeroes);
+  }
+  
+  extractList<T>(
+    fromSpecString: (specString: SpecString) => T | null,
+    listSizeMinBitLength: number | null = null,
+  ): T[] | null {
+    const count = listSizeMinBitLength === null
+      ? this.countZeroes()
+      : this.countZeroesAndReadNPlusBits(listSizeMinBitLength);
+    if (count === null) {
+      return null;
+    }
+    const list = [];
+    for (let i = 0; i < count; i++) {
+      const entity = fromSpecString(this);
+      if (entity === null) {
+        return null;
+      }
+      list.push(entity);
+    }
+    return list;
+  }
+}
