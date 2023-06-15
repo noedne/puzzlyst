a2993fc5e39c70eef607e6a36c36af7bbed6be5b
noedne <ney.brendan@gmail.com>
2023-06-14 22:37:08 -0700

    Add BaseCard class

---
+++ app/sdk/challenges/puzzleSpec/BaseCard.ts
@@ -0,0 +1,49 @@
+const Card = require('app/sdk/cards/card');
+const SDK = require('app/sdk');
+
+import type SpecString from './SpecString';
+
+import type { Group } from './CardGroup';
+import { getIdMinBitLength, getIdOffset } from './CardGroup';
+
+export default class BaseCard {
+  version: number;
+  group: Group;
+  id: number;
+  card: typeof Card | null = null;
+
+  constructor(version: number, group: Group, id: number) {
+    this.version = version;
+    this.group = group;
+    this.id = id;
+  }
+
+  static fromSpecString(specString: SpecString): BaseCard | null {
+    const version = specString.countZeroes();
+    if (version === null) {
+      return null;
+    }
+    const group = specString.matchRegex(/^(0[01]{3}|1[01])/) as Group | null;
+    if (group === null) {
+      return null;
+    }
+    const id = specString.countZeroesAndReadNPlusBits(getIdMinBitLength(group));
+    if (id === null) {
+      return null;
+    }
+    return new BaseCard(version, group, id);
+  }
+
+  getCardId(): number {
+    return this.id + getIdOffset(this.group);
+  }
+
+  getCard() {
+    if (this.card === null) {
+      this.card = SDK.GameSession.current().createCardForIdentifier(
+        this.getCardId(),
+      );
+    }
+    return this.card;
+  }
+}
