1b363ff90ce0c28c3afd357bfe117e7d6d8645c4
noedne <ney.brendan@gmail.com>
2023-06-14 22:39:04 -0700

    Add Modifier class

---
+++ app/sdk/challenges/puzzleSpec/Modifier.ts
@@ -0,0 +1,45 @@
+import BaseCard from "./BaseCard";
+import type SpecString from "./SpecString";
+
+const getContextObjectData = require('app/sdk/challenges/getContextObjectData');
+
+export default class Modifier {
+  baseCard;
+  indexOfContextObject;
+  multiplicity;
+
+  constructor(
+    baseCard: BaseCard,
+    indexOfContextObject: number,
+    multiplicity: number,
+  ) {
+    this.baseCard = baseCard;
+    this.indexOfContextObject = indexOfContextObject;
+    this.multiplicity = multiplicity;
+  }
+
+  static fromSpecString(specString: SpecString): Modifier | null {
+    const baseCard = BaseCard.fromSpecString(specString);
+    if (baseCard === null) {
+      return null;
+    }
+    const cardId = baseCard.getCardId();
+    const array = getContextObjectData(cardId, baseCard.version);
+    if (array.length === 0) {
+      return null;
+    }
+    const index = array.length === 1 ? 0 : specString.countZeroes();
+    if (index === null) {
+      return null;
+    }
+    const data = array[index];
+    if (data === undefined) {
+      return null;
+    }
+    const multiplicity = data.allowMultiple ? specString.countZeroes() : 0;
+    if (multiplicity === null) {
+      return null;
+    }
+    return new Modifier(baseCard, index, multiplicity);
+  }
+}
