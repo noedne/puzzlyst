dc8b79317d9ff7fa2e161cd6bb9b0975a990de4b
noedne <ney.brendan@gmail.com>
2023-06-17 04:51:14 -0700

    Add toString utils to SpecString

--- app/sdk/challenges/puzzleSpec/SpecString.ts
+++ app/sdk/challenges/puzzleSpec/SpecString.ts
@@ -21,12 +21,20 @@ export default class SpecString {
     return match.length - 1;
   }
 
+  static writeNZeroes(n: number): string {
+    return `${'0'.repeat(n)}1`;
+  }
+
   readNBits(n: number): number | null {
     const match = this.matchRegex(new RegExp(`^[01]{${n}}`));
     if (match === null) {
       return null;
     }
     return parseInt(match, 2);
+  }
+
+  static writeNumWithNBits(num: number, n: number): string {
+    return num.toString(2).padStart(n, '0');
   }
 
   countZeroesAndReadNPlusBits(n: number): number | null {
@@ -39,6 +47,12 @@ export default class SpecString {
       this.index--;
     }
     return this.readNBits(n + zeroes);
+  }
+
+  static padNumWithZeroesForCountingPastNMinBits(num: number, n: number): string {
+    const str = num.toString(2).padStart(n, '0');
+    const prefix = str.length === n ? '1' : '0'.repeat(str.length - n);
+    return `${prefix}${str}`;
   }
   
   extractList<T>(
@@ -60,5 +74,15 @@ export default class SpecString {
       list.push(entity);
     }
     return list;
+  }
+
+  static constructList<T>(
+    list: T[],
+    listSizeBitLength: number | null = null,
+  ): string {
+    return list.reduce((acc, val) => `${acc}${val}`,
+      listSizeBitLength === null
+        ? SpecString.writeNZeroes(list.length)
+        : SpecString.writeNumWithNBits(list.length, listSizeBitLength));
   }
 }
