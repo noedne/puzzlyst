fb3231aad1da225a162dbcf8f7a2b616301331fa
noedne <ney.brendan@gmail.com>
2023-07-03 09:38:47 -0700

    Add view/template for set damage modal

---
+++ app/ui/views/item/set_damage_modal.ts
@@ -0,0 +1,30 @@
+const Card = require('app/sdk/cards/card');
+const FormPromptModalItemView = require('./form_prompt_modal');
+const NavigationManager = require('app/ui/managers/navigation_manager');
+const SDK = require('app/sdk');
+const Template = require('app/ui/templates/item/set_damage_modal.hbs');
+import withNumberInput from './with_number_input';
+
+const NumberInputView = withNumberInput(FormPromptModalItemView);
+
+export default NumberInputView.extend({
+  id: 'app-set-damage',
+  template: Template,
+
+  initialize: function (options: { card: typeof Card }) {
+    this.card = options.card;
+    NumberInputView.prototype.initialize.apply(this, [{
+      initial: this.card.getDamage(),
+      max: this.card.getMaxHP() - 1,
+      min: 0,
+      placeholder: 0,
+      select: true,
+    }]);
+  },
+
+  onSubmitImpl: function (damage: number) {
+    SDK.GameSession.current().setCardDamage(this.card, damage);
+    NavigationManager.getInstance().destroyModalView();
+    this.trigger('submit');
+  },
+});
