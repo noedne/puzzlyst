9d0d9b5c6b5ddfbb0627730f697fb64c5a506187
noedne <ney.brendan@gmail.com>
2023-12-14 23:35:53 -0800

    Refactor number input html into class

--- app/ui/views/item/number_input.ts
+++ app/ui/views/item/number_input.ts
@@ -1,9 +1,17 @@
 export default class NumberInput {
-  private readonly ui: {
-    $numberInput: JQuery,
-    $upArrow: JQuery,
-    $downArrow: JQuery,
-  };
+  private readonly $upArrow = $(`
+    <button class="btn arrow up-arrow" tabindex="-1">
+      <i class="fa fa-2x fa-caret-up" />
+    </button>
+  `);
+  private readonly $numberInput = $(`
+    <input class="form-control number-input" type="number">
+  `);
+  private readonly $downArrow = $(`
+    <button class="btn arrow down-arrow" tabindex="-1">
+      <i class="fa fa-2x fa-caret-down" />
+    </button>
+  `);
   private readonly max: number;
   private readonly min: number;
   private readonly placeholder: number;
@@ -24,23 +32,19 @@ export default class NumberInput {
       select: boolean,
     },
   ) {
-    this.ui = {
-      $upArrow: $groupElement.children('.up-arrow'),
-      $numberInput: $groupElement.children('.number-input'),
-      $downArrow: $groupElement.children('.down-arrow'),
-    };
     this.max = max;
     this.min = min;
     this.placeholder = placeholder;
-    this.ui.$numberInput.attr({ max, min, placeholder });
-    this.ui.$numberInput.val(initial);
+    $groupElement.append(this.$upArrow, this.$numberInput, this.$downArrow);
+    this.$numberInput.attr({ max, min, placeholder });
+    this.$numberInput.val(initial);
     this.setDisabled();
     if (select) {
-      this.ui.$numberInput.trigger('select');
+      this.$numberInput.trigger('select');
     }
-    this.ui.$upArrow.on('click', this.onClickUpArrow.bind(this));
-    this.ui.$downArrow.on('click', this.onClickDownArrow.bind(this));
-    this.ui.$numberInput.on('input', this.onNumberChange.bind(this));
+    this.$upArrow.on('click', this.onClickUpArrow.bind(this));
+    this.$downArrow.on('click', this.onClickDownArrow.bind(this));
+    this.$numberInput.on('input', this.onNumberChange.bind(this));
   }
 
   public getIsValid(): boolean {
@@ -49,7 +53,7 @@ export default class NumberInput {
   }
 
   public getValue(): number {
-    const value = this.ui.$numberInput.val();
+    const value = this.$numberInput.val();
     if (typeof value !== 'string') {
       throw Error('invalid');
     }
@@ -69,22 +73,22 @@ export default class NumberInput {
 
   private onNumberChange(): void {
     if (this.getValue() < this.min) {
-      this.ui.$numberInput.val(this.min);
+      this.$numberInput.val(this.min);
     } else if (this.getValue() > this.max) {
-      this.ui.$numberInput.val(this.max);
+      this.$numberInput.val(this.max);
     }
     this.setDisabled();
   }
 
   private changeValue(delta: number): void {
-    this.ui.$numberInput.val(
+    this.$numberInput.val(
       Math.max(this.min, Math.min(this.getValue() + delta, this.max)),
     );
     this.onNumberChange();
   }
 
   private setDisabled(): void {
-    this.ui.$upArrow.prop('disabled', this.getValue() === this.max);
-    this.ui.$downArrow.prop('disabled', this.getValue() === this.min);
+    this.$upArrow.prop('disabled', this.getValue() === this.max);
+    this.$downArrow.prop('disabled', this.getValue() === this.min);
   }
 }
